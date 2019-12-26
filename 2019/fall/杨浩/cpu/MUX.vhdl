d71c34a4659279b98ed23cea0499ccc2dba28dd9
YH-1008 <59013343+YH-1008@users.noreply.github.com>
2019-12-26 17:21:58 +0800

    Create MUX.vhdl

---
+++ 2019/fall/杨浩/cpu/MUX.vhdl
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164. ALL;
+entity mux5 is
+port(
+A,B:in std_logic_vector(4 downto 0);
+s:in std_logic; 
+Z:out std_logic_vector(4 downto 0));
+end mux5;
+architecture Behavioral of mux5 is
+begin
+Z<=a when s='0'
+else b;
+end Behavioral;
